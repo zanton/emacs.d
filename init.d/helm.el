ed7c0f611fbe1f1033e99851f2d740cbead9147b
An Huynh <zanton.zzz@gmail.com>
2020-01-09 17:47:12 +0900

    Update helm.el

--- init.d/helm.el
+++ init.d/helm.el
@@ -3,6 +3,7 @@
 (add-to-list 'load-path (concat user-emacs-dir "/lisp/helm"))
 (require 'helm-config)
 (helm-mode 1)
+(setq helm-buffer-max-length nil)
 
 ;; make helm buffer always at bottom
 (defvar spacemacs-helm-display-help-buffer-regexp '("\\*.*Helm.*Help.*\\*"))
@@ -23,3 +24,4 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-map (kbd "M-h") 'backward-kill-word)
